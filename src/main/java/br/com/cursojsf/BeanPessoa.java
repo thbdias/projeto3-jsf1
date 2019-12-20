3e395556b5144bb095caff4ba5e1c57dda0138dc
thbdias <thiagobalbino91@gmail.com>
2019-12-20 10:41:22 -0300

    session scoped

--- src/main/java/br/com/cursojsf/BeanPessoa.java
+++ src/main/java/br/com/cursojsf/BeanPessoa.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.bean.ViewScoped;
 
 //@RequestScoped 
-@ViewScoped
+//@ViewScoped
+@SessionScoped
 @ManagedBean(name = "beanPessoa")
 public class BeanPessoa {
 
